c466ba50cc2cb5f8b6ec28202001f4785f7b87b1
muhammedmustafaturan <168023129+muhammedmustafaturan@users.noreply.github.com>
2024-05-01 16:40:25 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("hello world") 
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+            
+word = input("Anlamadığın kelimeyi benimle paylaş")            
+
+if word in meme_dict:
+    meaning = meme_dict[word]
+    print(f"{word} kelimesi şu anlama gelir)
